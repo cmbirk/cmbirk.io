b69f73a6a37403369a2cc3f11574d48f46fa9d22
Chris Birk <cmbirk@gmail.com>
2016-05-20 22:30:32 -0500

    add sidebar functionality

--- resources/assets/js/components/NavMenu.vue
+++ resources/assets/js/components/NavMenu.vue
@@ -1,10 +1,31 @@
 <template>
-  <nav>
+  <nav v-bind:class="{'active': navToggle}" v-on:click="toggleNav">
     <i class="fa fa-bars"></i>
+    <ul>
+      <li>
+        <a v-link="'/'">Home</a>
+      </li>
+      <li>
+        <a v-link="'/about'">About</a>
+      </li>
+      <li>
+        <a v-link="'/resume'">Resumé</a>
+      </li>
+    </ul>
   </nav>
 </template>
 <script>
   export default {
+    data () {
+      return {
+        navToggle: false
+      }
+    },
+    methods: {
+      toggleNav () {
+        this.navToggle = !this.navToggle;
+      }
+    },
     replace: false
   }
 </script>
